32ae9e26c98189a4535be696ad9b49487acc800b
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-09-30 16:47:05 -0700

    imported routes into app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import login from './pages/auth/login';
+import singUp from './pages/auth/signUp';
+import landingPage from './pages/landingPage';
+import dashboard from './pages/home/dashboard';
+import interviewPrep from './pages/interviewPrep/interviewPrep';
+
+
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+    </div>
   )
 }
 
