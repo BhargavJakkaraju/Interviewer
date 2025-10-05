13f7a6b09c39bb616cff9c5ee768a0036f21c2b1
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-05 16:01:46 -0700

    Login detail page

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -5,18 +5,18 @@ const Modal = ({ children, isOpen, onClose, title, hideHeader }) => {
     <div className='fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-black/40'>
       <div className='relative flex flex-col bg-white shadow-lg rounded-lg overflow-hidden'>
         {!hideHeader && (
-          <div className=''>
-            <h3 className='title'>{title}</h3>
+          <div className='flex items-center justify-between p-4 border-b border-gray-200'>
+            <h3 className='md:text-lg font-medium text-gray-900'>{title}</h3>
           </div>
         )}
 
         <button
           type='button'
-          className=''
+          className='text-gray-400 bg-transparent hover:bg-orange-100 hover:text-gray-900 rouned-lg text-sm w-8 h-8 flex justify-center items-center absolute top-3.5 right-3.5 cursor-pointer'
           onClick={onClose}
         >
           <svg
-            className=''
+            className='w-3 h-3'
             aria-hidden='true'
             xmlns='http://www.w3.org/2000/svg'
             fill='none'
@@ -32,7 +32,7 @@ const Modal = ({ children, isOpen, onClose, title, hideHeader }) => {
           </svg>
         </button>
 
-        <div className=''>
+        <div className='felx-1 overflow-y-auto custom-scrollbar'>
           {children}
         </div>
       </div>
--- frontend/src/pages/auth/login.jsx
+++ frontend/src/pages/auth/login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
+const Login = ({setCurrentPage}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
+  return <div className='w-[90vw] md:w:w=[33vw] p-7 flex-col justify-center'>
+    <h3 className=''>Welcome Back</h3>
+    <p className=''>
+      Enter your credentials to log in
+    </p>
+
+    <form onSubmit={handleLogin}>
+
+
+    </form>
+
+  </div>
 }
 
 export default Login
