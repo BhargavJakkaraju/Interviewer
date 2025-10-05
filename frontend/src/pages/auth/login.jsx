3d59300d6e98e8ffe112bdc0a925c99b894a1f55
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-05 16:17:33 -0700

    Input error with imports fixed

--- frontend/src/pages/auth/login.jsx
+++ frontend/src/pages/auth/login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Input from '../../components/inputs/Input';
 
 const Login = ({setCurrentPage}) => {
   const [email, setEmail] = useState('');
@@ -12,17 +13,29 @@ const Login = ({setCurrentPage}) => {
     e.preventDefault();
   };
 
-  return <div className='w-[90vw] md:w:w=[33vw] p-7 flex-col justify-center'>
-    <h3 className=''>Welcome Back</h3>
-    <p className=''>
+  return <div className='w-[90vw] md:w=[33vw] p-7 flex-col justify-center'>
+    <h3 className='text-lg font-semibold text-black'>Welcome Back</h3>
+    <p className='text-xs text-slate-700 mt-5 mb-6'>
       Enter your credentials to log in
     </p>
 
     <form onSubmit={handleLogin}>
+      <Input 
+        value={email}
+        onChange={({ target }) => setEmail(target.value)}
+        label='Email Address'
+        placeHolder="name@email.com"
+        type='text'
+      />
 
-
+      <Input
+        value={password}
+        onChange={({ target }) => setPassword(target.value)}
+        label='Password'
+        placeHolder="Minimum 8 Characters"
+        type='password'
+      />
     </form>
-
   </div>
 }
 
