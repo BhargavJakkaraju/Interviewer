460250784a624af20a5976c2565c8f2dfe885a89
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-10 14:55:36 -0700

    Added login user function to auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -37,6 +37,27 @@ const registerUser = async (req, res) => {
 }
 
 const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({ message: "User does not exist "})
+        }
+
+        const match = await bcrypt.compare(password, user.password)
+        if (!match) {
+            return res.status(400).json({ message: "Invalid Password"})
+        }
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user.id)
+        })
+    } catch(error) {
+        res.status(500).json({ message: "Internal Server Error", error})
+    }
 
 }
 
