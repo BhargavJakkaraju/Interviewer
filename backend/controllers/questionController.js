e8e215ddbdd09ade42a7856c1d9722f275fd0af2
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-11 19:07:44 -0700

    reolved failure due to unadded exports in questionController

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -1,2 +1,26 @@
 const Question = require('../models/Question')
 const Session = require('../models/Session')
+
+exports.addQuestionsToSession = async (req, res) => {
+    try {
+
+    } catch( error) {
+        res.status(500).json({ success: false,  message: "Internal Server error" })
+    }
+}
+
+exports.togglePinQuestions = async (req, res) => {
+    try {
+
+    } catch( error) {
+        res.status(500).json({ success: false,  message: "Internal Server error" })
+    }
+}
+
+exports.updateQuestionNote = async (req, res) => {
+    try {
+
+    } catch( error) {
+        res.status(500).json({ success: false,  message: "Internal Server error" })
+    }
+}
