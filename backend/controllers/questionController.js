552b558561d45e78f7b8465cf6d40e36a7bbc869
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-14 11:54:48 -0700

    added feature to add questions in questions controller

--- backend/controllers/questionController.js
+++ backend/controllers/questionController.js
@@ -3,7 +3,30 @@ const Session = require('../models/Session')
 
 exports.addQuestionsToSession = async (req, res) => {
     try {
+        const { sessionId, questions } = req.body
 
+        if (!sessionId || !questions || !Array.isArray(questions)) {
+            return res.status(400).json({ message: "Invalid Input Data"})
+        }
+
+        const session = await Session.findById(sessionId);
+
+        if (!session) {
+            return res.status(404).json({ message: "Session not found"})
+        }
+
+        const createdQuesetions = await Question.insertMany(
+            questions.map((q) => ({
+                session: sessionId, 
+                question: q.question,
+                answer: q.answer
+            }))
+        )
+
+        session.questions.push(...createdQuesetions.map((q) => q._id));
+        await session.save()
+
+        res.status(201).json(createdQuesetions)
     } catch( error) {
         res.status(500).json({ success: false,  message: "Internal Server error" })
     }
