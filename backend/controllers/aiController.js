8105099e2cd8b8ef3d248ca70f61f1557e30d1f3
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-14 20:24:28 -0700

    completed aiController skeleton and fixed server import error

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -2,3 +2,15 @@ const { GoogleGenAI } = require('@google/genai')
 const { conceptExplainPrompt } = require('../utils/prompts')
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
+
+const generateInterviewQuestions = async (req, res) => {
+    try {
+
+    } catch(error) {
+        res.status(500).json({ message: "Failed to generate questions", error: error.message})
+    }
+}
+
+const generateConceptExplanation = async (req, res) => {}
+
+module.exports = { generateConceptExplanation, generateInterviewQuestions}
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const authRoutes = require('./routes/authRoutes')
 const sessionRoutes = require('./routes/sessionRoutes')
 const questionRoutes = require('./routes/questionRoutes')
 const { protect } = require('./middlewares/authMiddleware')
+const { generateConceptExplanation, generateInterviewQuestions } = require('./controllers/aiController')
 
 const app = express();
 
