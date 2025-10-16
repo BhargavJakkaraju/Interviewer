e46dfd694731ae8deab6105fcd9c5b6b80a8ad8a
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-15 21:33:32 -0700

    Added prompt for gemini and exported successfully

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -5,7 +5,7 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
 
 const generateInterviewQuestions = async (req, res) => {
     try {
-
+        
     } catch(error) {
         res.status(500).json({ message: "Failed to generate questions", error: error.message})
     }
