28efbb59be036e418490b407e2da65f0bb973e89
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-16 21:42:10 -0700

    added axiosInstance function

---
+++ frontend/src/utils/axiosInstance.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import { BASE_URL } from './apiPaths'
+import { application } from 'express'
+
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    timeout: 80000,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+    }
+})
