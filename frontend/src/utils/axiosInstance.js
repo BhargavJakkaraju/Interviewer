2cc7e614cb02e44b6aefc3bdcf9be6c3cc9daa42
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-16 21:55:53 -0700

    Added response interceptor

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -23,3 +23,21 @@ axiosInstance.interceptors.request.use(
         return Promise.reject(error);
     }
 )
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    (error) => {
+        if (error.response) {
+            if (error.response.status === 401) {
+                window.location.href = '/'
+            } else if (error.response.status === 500) {
+                console.error("Server Error. Please try again later")
+            }
+        } else if (error.code === "ECONNABORTED") {
+            console.error("Request timeout. Please try again")
+        }
+        return Promise.reject(error)
+    }
+)
