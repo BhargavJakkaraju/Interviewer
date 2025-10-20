a8ff75cb6f71e48dd7ae602b2fdbdb27f1843838
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-20 13:21:00 -0700

    Completed useEffect for userContext

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -1,10 +1,39 @@
 import React, { createContext, useState, useEffect } from 'react'
 import axiosInstance from '../utils/axiosInstance'
 import { API_PATHS } from '../utils/apiPaths'
+import axios from 'axios'
 
 export const UserContext = createContext()
 
 const UserProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        if (user) return
+
+        const accessToken = localStorage.getItem("token")
+        if (!accessToken) {
+            setLoading(false)
+            return
+        }
+
+        const fetchUser = async () => {
+            try{
+                const response = await axiosInstance.get(API_PATHS.AUTH.GET_PROFILE)
+                setUser(response.data)
+            } catch(error) {
+                console.error("User not authorized", error)
+                clearuser()
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchUser();
+    }, [])
+
     return (
         <UserContext.Provider value={{ user, loading, updateUser, clearuser}}>
             {children}
