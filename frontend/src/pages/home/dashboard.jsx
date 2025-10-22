e038d1472f1f0cc1ce8006384d9ee5fe5f5fa2e9
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-22 15:50:58 -0700

    fixed dashboard import bug

--- frontend/src/pages/home/dashboard.jsx
+++ frontend/src/pages/home/dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { LuPlus } from 'react-icons/lu'
 import { CARD_BG } from "../../utils/data"
 import toast from "react-hot-toast"
+import DashboardLayout from '../../components/layouts/DashboardLayout'
 
 const Dashboard = () => {
   return (
