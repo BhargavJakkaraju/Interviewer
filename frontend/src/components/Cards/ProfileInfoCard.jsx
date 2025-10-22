14f1a723d94bd40c71f8d1c9b63c04a00635ef38
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-21 17:31:44 -0700

    Added logout feature to homepage

--- frontend/src/components/Cards/ProfileInfoCard.jsx
+++ frontend/src/components/Cards/ProfileInfoCard.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../../context/UserContext'
+import { useNavigate } from 'react-router-dom'
 
 const ProfileInfoCard = () => {
+    const { user, clearuser} = useContext(UserContext)
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.clear()
+        clearuser();
+        navigate('/')
+    }
   return (
-    <div>ProfileInfoCard</div>
+    <div className='flex items-center'>
+        <div>
+            <div className='text-[15px] text-black font-bold loading-3'>
+                {user.name || ''}
+            </div>
+            <button className='text-amber-600 text-sm font-semibold cursor-pointer hover:underline' onClick={handleLogout}> Logout </button>
+        </div>
+    </div>
   )
 }
 
