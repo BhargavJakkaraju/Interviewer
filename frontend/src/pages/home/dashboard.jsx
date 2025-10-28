0978754bdc7920033a822fa14b1c2ba8b5ecbbe3
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-27 19:35:52 -0700

    dashboard navigate system update

--- frontend/src/pages/home/dashboard.jsx
+++ frontend/src/pages/home/dashboard.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { LuPlus } from 'react-icons/lu'
 import { CARD_BG } from "../../utils/data"
 import toast from "react-hot-toast"
 import DashboardLayout from '../../components/layouts/DashboardLayout'
+import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
+  const navigate = useNavigate()
+  const [openCreateModal, setOpenCreateModal] = useState(false)
+  const [session, setSession] = useState({
+    open: false,
+    data: null
+})
+
+const fetchAllSessions = async () => {}
+
+useEffect(() => {
+  fetchAllSessions()
+})
   return (
     <DashboardLayout>Dashboard</DashboardLayout>
   )
