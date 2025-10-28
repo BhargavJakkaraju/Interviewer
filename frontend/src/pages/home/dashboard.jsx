2e9b1e7b4c19e3fcb1866c1c783197b9f5089b34
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-10-27 19:48:26 -0700

    Added 'add new' button to dashboard

--- frontend/src/pages/home/dashboard.jsx
+++ frontend/src/pages/home/dashboard.jsx
@@ -19,7 +19,18 @@ useEffect(() => {
   fetchAllSessions()
 })
   return (
-    <DashboardLayout>Dashboard</DashboardLayout>
+    <DashboardLayout>
+      <div className='conrainer mx-auto pt-4 pb-4'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-7 pt-1 pb-6 px-4 md:px-6'>
+
+        </div>
+
+        <button className='h-12 flex items-center justify-center gap-3 bg-gradient-to-r from-[#FF9324] to-[#e99a4b] text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white transition-colors cursor-pointer hover:shadow-2xl hover:shadow-orange-300 fixed bottom-10 md:bottom-20 md:right-20' onClick={() => setOpenCreateModal(true)}>
+          <LuPlus className='' />
+          Add New
+        </button>
+      </div>
+    </DashboardLayout>
   )
 }
 
