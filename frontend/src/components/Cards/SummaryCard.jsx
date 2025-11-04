219133bfe9e9d9e9f84cc64e6b36a896e6986ef1
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-11-03 20:13:34 -0800

    added basic skeleton of summary card

--- frontend/src/components/Cards/SummaryCard.jsx
+++ frontend/src/components/Cards/SummaryCard.jsx
@@ -11,7 +11,66 @@ const SummaryCard = ({
     onSelect,
     onDelete,
 }) => {
-  return <div>SummaryCard</div>
+  return  <div 
+    className='' 
+    onClick={onSelect}
+  >
+    <div 
+      className=''
+      style={{
+        background: colors.bgcolor,
+      }}
+    >
+      <div className=''>
+        <div className=''>
+          <span className=''>
+            GU
+          </span>
+        </div>
+
+        <div className=''>
+          <div className=''>
+            <div>
+              <h2 className=''>{role}</h2>
+              <p className=''>
+                {topicsToFocus}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <button
+        className=''
+        onClick={(e) => {
+          e.stopPropagation()
+          onDelete()
+        }}
+      >
+        delete
+      </button>
+    </div>
+
+    <div className=''>
+      <div className=''>
+        <div className=''>
+          Experience: {experience} {experience == 1 ? "Year" : "Year"}
+        </div>
+
+        <div className=''>
+          {questions} Q&A
+        </div>
+
+        <div className=''>
+          Last Updated: {lastUpdated}
+        </div>
+      </div>
+
+      <p className=''>
+        {description}
+      </p>
+    </div>
+  </div>
 }
 
 export default SummaryCard
