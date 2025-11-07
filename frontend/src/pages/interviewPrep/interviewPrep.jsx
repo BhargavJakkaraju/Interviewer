dd827dc8cb1a64568330cfe314b3339a30ba7759
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-11-06 20:09:31 -0800

    bug

--- frontend/src/pages/interviewPrep/interviewPrep.jsx
+++ frontend/src/pages/interviewPrep/interviewPrep.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import moment from 'moment'
 import {AnimatePresence, motion} from 'framer-motion'
 import { LuCircleAlert, LuListCollage} from 'react-icons/lu'
+import { toast } from 'react-hot-toast'
 
 const InterviewPrep = () => {
   return (
