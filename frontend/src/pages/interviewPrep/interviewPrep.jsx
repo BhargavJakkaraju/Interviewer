71630fcdf05dcf3262a07c7a9dc89dc2f08c3832
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-11-06 20:09:02 -0800

    fixed imports

--- frontend/src/pages/interviewPrep/interviewPrep.jsx
+++ frontend/src/pages/interviewPrep/interviewPrep.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import moment from 'moment'
 import {AnimatePresence, motion} from 'framer-motion'
+import { LuCircleAlert, LuListCollage} from 'react-icons/lu'
 
 const InterviewPrep = () => {
   return (
