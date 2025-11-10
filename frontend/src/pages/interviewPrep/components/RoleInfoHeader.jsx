ae0e14c014c5e24a27a307c915bd715b40213352
BhargavJakkaraju <Bhargavjakkaraju@gmail.com>
2025-11-10 14:08:05 -0800

    added styling to role info header

--- frontend/src/pages/interviewPrep/components/RoleInfoHeader.jsx
+++ frontend/src/pages/interviewPrep/components/RoleInfoHeader.jsx
@@ -8,15 +8,15 @@ const RoleInfoHeader = ({
     questions,
     lastUpdated
 }) => {
-  return <div className=''>
-    <div className=''>
-      <div className=''>
-        <div className=''>
-          <div className=''>
-            <div className=''>
-              <div className=''>
-              <h2 className=''>{role}</h2>
-              <p className=''>
+  return <div className='bg-white relative'>
+    <div className='container mx-auto px-10 md:px-0'>
+      <div className='h-[200px] flex flex-col justify-center relative z-10'>
+        <div className='flex items-start'>
+          <div className='flex-grow'>
+            <div className='flex justify-between items-start'>
+              <div>
+              <h2 className='text-2xl font-medium'>{role}</h2>
+              <p className='text-sm text-medium text-gray-900 mt-1'>
                 {topicsToFocus}
               </p>
             </div>
@@ -24,26 +24,26 @@ const RoleInfoHeader = ({
         </div>
       </div>
 
-      <div className=''>
-        <div className=''>
+      <div className='flex items-center gap-3 mt-4'>
+        <div className='text-[10px] font-semibold text-white bg-black ox-3 py-1 rounded-full'>
           Expereince: {experience == 1 ? "Year": "Years"}
         </div>
 
-        <div className=''>
+        <div className='text-[10px] font-semibold text-white bg-black px-3 py-1 rounded-full'>
           {questions} Q&A
         </div>
 
-        <div className=''>
+        <div className='text-[10px] font-semibold text-white bg-black px-3 py-1 rounded-full'>
           Last Updated: {lastUpdated}
         </div>
       </div>
     </div>
 
-    <div className=''>
-      <div className='' />
-      <div className='' />
-      <div className='' />
-      <div className='' />
+    <div className='w-[40vw] md:w-30[vw] h-[200px] flex items-center justify-center bg-white overglow-hidden absolute top-0 right-0'>
+      <div className='w-16 h-16 bg-lime-400 blur-[65px] animate-blob1' />
+      <div className='w-16 h-16 bg-teal-400 blur-[65px] animate-blob2' />
+      <div className='w-16 h-16 bg-cyan-400 blur-[65px] animate-blob3' />
+      <div className='w-16 h-16 bg-fuchsia-400 blur-[65px] animate-blob4'/>
     </div>
   </div>
 </div>
